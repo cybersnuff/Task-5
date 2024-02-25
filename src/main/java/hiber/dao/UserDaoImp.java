c4b38eac5cea5a1196739c11908c2a8fa22b2558
Vadim <cybersnuff@yandex.ru>
2024-02-25 15:55:37 +0400

    commit

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -11,19 +11,38 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
+    public User getUserModelSeries(String model, int series) {
+        String hql = "FROM User u WHERE u.car.model = :model AND u.car.series = :series";
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql);
+        query.setParameter("model", model);
+        query.setParameter("series", series);
+        List<User> users = query.getResultList();
+        if (users.isEmpty()) {
+            System.out.println("Юзер с моделью " + model + " и серией " + series + " - не найден");
+            return null;
+        } else {
+            User user = users.get(0);
+            System.out.println("Пользователь с моделью " + model + " и серией " + series +
+                    " - " + user.getFirstName() + " "+
+                    " - "+ user.getLastName() +
+                    " -  " + user.getEmail());
+            return user;
+        }
+
+    }
 }
